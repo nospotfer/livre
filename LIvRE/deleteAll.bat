0d466aebe1bb5abfd50bba7e3b3951af9bd5f059
nospotfer <gabriel.barra@gmail.com>
2016-02-20 17:56:55 +0100

    many updates and fixes

--- LIvRE/deleteAll.bat
+++
@@ -1,2 +0,0 @@
-curl http://localhost:8983/solr/LIvRE/update  -H "Content-Type: text/xml" --data-binary "<delete><query>*:*</query></delete>"
-curl http://localhost:8983/solr/LIvRE/update  -H "Content-Type: text/xml" --data-binary "<commit/>"
